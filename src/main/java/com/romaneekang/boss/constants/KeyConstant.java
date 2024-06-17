f33043152286f3b9413d482b6e0a779829782da2
romaneekang <weikang0324@163.com>
2024-06-17 18:02:11 +0800

    feat(redis): redis定义key名称的常量类

---
+++ src/main/java/com/romaneekang/boss/constants/KeyConstant.java
@@ -0,0 +1,9 @@
+package com.romaneekang.boss.constants;
+
+public class KeyConstant {
+    /**
+     * 登录的key
+     * redis key的规范:    项目名：模块名:功能名:唯一业务参数名(用户id)
+     */
+    public static final String OPERATOR_LOGIN = "boss:operator:login";
+}
