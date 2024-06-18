4f8856f8347f4f8dedae101a7b421e6546aae774
romaneekang <weikang0324@163.com>
2024-06-18 21:42:50 +0800

    feat(Cors):全局跨域springmvc设置

--- src/main/java/com/romaneekang/boss/settings/WebMvcSettings.java
+++ src/main/java/com/romaneekang/boss/settings/WebMvcSettings.java
@@ -21,4 +21,18 @@ public class WebMvcSettings implements WebMvcConfigurer {
                 .addPathPatterns("/**") //拦截地址,所有请求都拦截
                 .excludePathPatterns("/operator/login");    //登录请求不拦截
     }
+
+    /**
+     * 跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 拦截的请求地址，哪些地址需要进行跨域拦截
+                .allowedOriginPatterns("*") // 请求的来源域名
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")  // 允许使用的请求方式
+                .allowCredentials(true) // 支持cookie
+                .maxAge(3600)   // 域检有效期
+                .allowedHeaders("*");   // 允许出现的请求header的名称
+
+    }
 }
